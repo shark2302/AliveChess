f29813ec04c1e30798b4dfb08f0caf04280121c5
e.kravchenko <e.kravchenko@corp.mail.ru>
2020-12-17 17:13:56 +0300

    Движение, генерация карты, захват замков

--- AliveChess/Assets/Scripts/Cell.cs
+++ AliveChess/Assets/Scripts/Cell.cs
@@ -8,7 +8,8 @@ public class Cell : MonoBehaviour
 {
 
     public Action<Vector2> ClickEvent;
-    
+
+    public int SlowEffect;
     private void OnEnable()
     {
         ClickEvent = vector2 => {};
@@ -17,5 +18,25 @@ public class Cell : MonoBehaviour
     private void OnMouseDown()
     {
         ClickEvent.Invoke(transform.position);
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var king = other.GetComponent<Player>();
+        if (king != null)
+        {
+            king.SlowEffect = SlowEffect;
+            
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        var king = other.collider.GetComponent<Player>();
+        if (king != null)
+        {
+            king.SlowEffect = SlowEffect;
+            
+        }
     }
 }
--- AliveChess/Assets/Scripts/GameManager.cs
+++ AliveChess/Assets/Scripts/GameManager.cs
@@ -2,28 +2,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
 
-    public GameObject CellPrefab;
+    public GameObject TowerPrefab;
+
+    public GameObject[] CellPrefabs;
 
     public GameObject KingPrefab;
 
-    public int DeskSize;
+    public Text TowerCountText;
+
+    public Text PlayerRankText;
     
-    private List<KingMovement> _kingsMovements;
+    public int DeskSize;
+
+    public int TowerCount;
+
+    public FollowCamera Camera;
+    
+    private List<Player> _kingsMovements;
     
     private List<Cell> _cells;
-    public List<KingMovement> KingsMovements => _kingsMovements;
-
+    private List<Tower> _towers;
     private void OnEnable()
     {
-        _kingsMovements = new List<KingMovement>(4);
+        TowerCountText.text = string.Empty;
+        PlayerRankText.text = "Ранк игрока : " + PlayerPrefs.GetInt("PlayerRank");
+        _kingsMovements = new List<Player>(4);
         _cells = new List<Cell>();
+        _towers = new List<Tower>(TowerCount);
         SpawnKing();
         SpawnCells();
-        
+        SpawnTowers();
+        Camera.SetFollowObject(_kingsMovements[0].gameObject);
     }
 
     private void OnDisable()
@@ -32,19 +47,36 @@ public class GameManager : MonoBehaviour
         {
             UnsubscribeCell(cell);
         }
-    }
 
+        foreach (var tower in _towers)
+        {
+            UnsubscribeKingOnTowerClick(tower);
+        }
+        DestroyAll();
+    }
+    
 
     private void SpawnCells()
     {
         var posX = transform.position.x;
         var posy = transform.position.y;
+        
+        var oneTypeCell = Random.Range(3, 12);
+        var spawningCellPrefab = CellPrefabs[Random.Range(0, CellPrefabs.Length)];
+        
         for (int i = 0; i < DeskSize; i++)
         {
             for (int j = 0; j < DeskSize; j++)
             {
-                var go = Instantiate(CellPrefab, new Vector3(posX, posy), Quaternion.identity, transform);
+                if (oneTypeCell == 0)
+                {
+                    oneTypeCell = Random.Range(3, 12);
+                    spawningCellPrefab = CellPrefabs[Random.Range(0, CellPrefabs.Length)];
+                }
+                
+                var go = Instantiate(spawningCellPrefab, new Vector3(posX, posy), Quaternion.identity, transform);
                 var cell = go.GetComponent<Cell>();
+                
                 if (cell != null)
                 {
                     _cells.Add(cell);
@@ -52,6 +84,7 @@ public class GameManager : MonoBehaviour
                 }
 
                 posy += 11;
+                oneTypeCell--;
             }
             posX += 11;
             posy = transform.position.y;
@@ -60,10 +93,58 @@ public class GameManager : MonoBehaviour
 
     private void SpawnKing()
     {
-        var king = Instantiate(KingPrefab, transform.position, Quaternion.identity);
-        var movement = king.GetComponent<KingMovement>();
-        if(movement != null)
+        var king = Instantiate(KingPrefab, new Vector3(transform.position.x + (Random.Range(0, 25) * 11),
+            transform.position.y + (Random.Range(0, 25) * 11)), Quaternion.identity);
+        var movement = king.GetComponent<Player>();
+        if (movement != null)
+        {
             _kingsMovements.Add(movement);
+            movement.TowerHoldedEvent += i =>
+            {
+                TowerCountText.text = "Захвачено башен : " + i;
+                if (i == TowerCount)
+                {
+                    RankUp();
+                }
+            };
+
+        }
+    }
+
+    private void SpawnTowers()
+    {
+        var xCell = Random.Range(2, 7);
+        var yCell = Random.Range(2, 7);
+        var tCount = TowerCount;
+        while (tCount > 0)
+        {
+            var go = Instantiate(TowerPrefab, new Vector3(xCell * 11 + 3, yCell * 11 - 2), Quaternion.identity);
+            var tower = go.GetComponent<Tower>();
+            SubscribeKingOnTowerClick(tower);
+            _towers.Add(tower);
+            var rand = Random.Range(0, 2);
+            switch (rand)
+            {
+                case 0:
+                    xCell += Random.Range(2,7);
+                    break;
+                case 1:
+                    yCell += Random.Range(2, 7);
+                    break;
+                case 2:
+                    xCell += Random.Range(2, 7);
+                    yCell += Random.Range(2, 7);
+                    break;
+            }
+            tCount--;
+        }
+    }
+
+    public void RankUp()
+    {
+        PlayerPrefs.SetInt("PlayerRank", PlayerPrefs.GetInt("PlayerRank") + 1);
+        OnDisable();
+        OnEnable(); 
     }
 
     private void SubscribeCellOnKingMovement(Cell cell)
@@ -82,6 +163,38 @@ public class GameManager : MonoBehaviour
         }    
     }
 
+    private void SubscribeKingOnTowerClick(Tower tower)
+    {
+        foreach (var king in _kingsMovements)
+        {
+            tower.TowerClickEvent += king.OnTowerClicked;
+        }    
+    }
+    
+    private void UnsubscribeKingOnTowerClick(Tower tower)
+    {
+        foreach (var king in _kingsMovements)
+        {
+            tower.TowerClickEvent -= king.OnTowerClicked;
+        }    
+    }
 
+    private void DestroyAll()
+    {
+        foreach (var cell in _cells)
+        {
+            Destroy(cell?.gameObject);
+        }
+
+        foreach (var king in _kingsMovements)
+        {
+            Destroy(king?.gameObject);
+        }
+
+        foreach (var tower in _towers)
+        {
+            Destroy(tower?.gameObject);
+        }
+    }
 
 }
